4d2c9427be11bffb95e26f07de1b2c84d36aa1a3
David Rodriguez <rodriguezd11@uabc.edu.mx>
2022-04-07 18:23:44 -0700

    Agregar archivos de proyecto.

---
+++ While/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String contra1, contra2 = "";
+            Console.WriteLine("Escribe tu contrasena: ");
+            contra1 = Console.ReadLine();
+            while(contra1 != contra2)
+            {
+                Console.WriteLine("Confirma tu contrasena");
+                contra2 = Console.ReadLine();
+            }
+            Console.WriteLine("Contasena gurdada");
+            Console.ReadKey();
+        }
+    }
+}
